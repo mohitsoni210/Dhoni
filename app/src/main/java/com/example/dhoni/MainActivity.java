268780339164ff2fc0879d1713402c12c9006995
MOHIT <mohitswarnkar210@gmail.com>
2020-02-25 16:57:23 +0530

    hii

---
+++ app/src/main/java/com/example/dhoni/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.dhoni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void ram(View v)
+    {
+        Intent i=new Intent(this,SecondActivity.class);
+        startActivity(i);
+    }
+}
